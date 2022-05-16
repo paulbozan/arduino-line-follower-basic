373693896f20bdc7c2dae3ee72739959c146e3e8
DaMon-14 <84168247+DaMon-14@users.noreply.github.com>
2022-05-16 18:03:48 +0300

    Update sketch_dec30a.ino

--- src/sketch_dec30a/sketch_dec30a.ino
+++ src/sketch_dec30a/sketch_dec30a.ino
@@ -13,9 +13,9 @@ const unsigned int IN1_B = 3;
 const unsigned int IN2_B = 2;
 const unsigned int EN_B = 9;
 
-const unsigned int mspeed = 170;//motor speed forward
-const unsigned int mspeed_rot = 230; //motot speed when adjust the robot position
-const unsigned int mdelay = 120; //waiting time for a new loop to start.
+const unsigned int mspeed = 180;//motor speed forward
+const unsigned int mspeed_rot = 235; //motot speed when adjust the robot position
+const unsigned int mdelay = 1; //waiting time for a new loop to start.
 //defining pins and variables
 #define left A1
 #define center A2
@@ -33,73 +33,33 @@ void setup()
   
   // Used to display information
   Serial.begin(9600);
-
-  // Wait for Serial Monitor to be opened
-  while (!Serial)
-  {
-    //do nothing
-  }
-
   // Set initial speed for both motors
   motors.setSpeed(200);
 }
 
 void loop(){
-  //printing values of the sensors to the serial monitor
-  Serial.println("-----");
-  Serial.print("L:");
-  Serial.println(digitalRead(left));
-  Serial.print("C:");
-  Serial.println(digitalRead(center));
-  Serial.print("R:");
-  Serial.println(digitalRead(right));
-
   //line not detected by both
   if(digitalRead(left)==1 && digitalRead(right)==1){
-    Serial.println("line not detected by both");
-    //Forward
     motors.setSpeed(mspeed);
     motors.forward();
     
   }
   //line detected by left sensor
   else if(digitalRead(left)==0 && digitalRead(right)==1){
-    Serial.println("line detected by left");
-    //turn left
     motors.setSpeed(mspeed_rot);
     motors.forwardA();
     motors.backwardB();    
   }
   //line detected by right sensor
   else if(digitalRead(left)==1 && digitalRead(right)==0){
-    Serial.println("line detected by right");
-    //turn right
     motors.setSpeed(mspeed_rot);
     motors.forwardB();
     motors.backwardA();   
   }
   //line detected by both
   else if(digitalRead(left)==0 && digitalRead(right)==0){
-    Serial.println("stop");
-    //stop
     motors.stop();   
   }
   delay(mdelay);
   motors.stop();
-  printSomeInfo();
-}
-
-/*
-Print some informations in Serial Monitor
-*/
-void printSomeInfo()
-{
-  Serial.print("Motor A is moving = ");
-  Serial.print(motors.isMovingA() ? "YES" : "NO");
-  Serial.print(" at speed = ");
-  Serial.println(motors.getSpeedA());
-  Serial.print("Motor B is moving = ");
-  Serial.print(motors.isMovingB() ? "YES" : "NO");
-  Serial.print(" at speed = ");
-  Serial.println(motors.getSpeedB());
 }
